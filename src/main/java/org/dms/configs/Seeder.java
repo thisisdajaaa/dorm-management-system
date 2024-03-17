d19d84be802a670a4983c618abc71febac09a795
natboltu <madhusodanopu@gmail.com>
2024-03-17 16:43:35 -0500

    seeding room and room assignment

--- src/main/java/org/dms/configs/Seeder.java
+++ src/main/java/org/dms/configs/Seeder.java
@@ -4,8 +4,15 @@ import org.dms.annotations.Autowired;
 import org.dms.annotations.Component;
 import org.dms.models.Person;
 import org.dms.constants.Role;
+import org.dms.models.Room;
 import org.dms.services.spec.IKeyService;
 import org.dms.services.spec.IPersonService;
+import org.dms.services.spec.IRoomAssignmentService;
+import org.dms.services.spec.IRoomService;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class Seeder {
@@ -14,10 +21,35 @@ public class Seeder {
 
     @Autowired
     private IPersonService personService;
+    @Autowired
+    private IRoomService roomService;
+    @Autowired
+    private IRoomAssignmentService roomAssignmentService;
 
     public void run() {
         seedAdmins();
         seedKeys();
+        seedRooms();
+        seedRoomAssignment();
+    }
+
+    private void seedRoomAssignment() {
+        List<Map.Entry<Integer, Person>> persons = personService.findAll();
+
+        persons.stream()
+                .map(entry -> entry.getValue())
+                .forEach(person -> {
+                    if(roomService.checkInRoom().isPresent()) {
+                        Room room = roomService.checkInRoom().get();
+                        roomAssignmentService.addRoomAssignment(room.roomNumber(), person.getId(),
+                                LocalDate.of(2023,02,21), LocalDate.of(202,01,01));
+                    }
+                });
+    }
+
+    private void seedRooms() {
+        roomService.addRoom(101);
+        roomService.addRoom(102);
     }
 
     public void seedAdmins() {
