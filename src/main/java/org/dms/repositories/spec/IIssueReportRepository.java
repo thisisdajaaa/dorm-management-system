143e79226da302d3e04d3d5d1c29abce1af77add
Marc-Colina <marccolina456@gmail.com>
2024-03-17 16:11:25 -0500

    Update IssueReport interface to extend IBase interface

--- src/main/java/org/dms/repositories/spec/IIssueReportRepository.java
+++ src/main/java/org/dms/repositories/spec/IIssueReportRepository.java
@@ -1,14 +1,5 @@
 package org.dms.repositories.spec;
 
 import org.dms.models.IssueReport;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
-public interface IIssueReportRepository {
-
-    public void save(IssueReport issueReport);
-    public Optional<IssueReport> findById(Integer id);
-    List<Map.Entry<Integer, IssueReport>> findAll();
+public interface IIssueReportRepository extends IBaseRepository<IssueReport>{
 }
