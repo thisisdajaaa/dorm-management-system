4968a29dc0481361b7a028e6f7eff3d67d1eef49
Marc-Colina <marccolina456@gmail.com>
2024-03-20 20:53:37 -0500

    Initial Changes to RoomScreen

--- src/main/java/org/dms/App.java
+++ src/main/java/org/dms/App.java
@@ -3,6 +3,9 @@ package org.dms;
 import org.dms.configs.Injector;
 import org.dms.configs.Seeder;
 import org.dms.views.Main;
+import org.dms.views.student.RoomScreen;
+
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
@@ -14,6 +17,9 @@ public class App {
 
 
         Main main = new Main();
-        main.executeView();
+       // main.executeView();
+        Scanner scanner = new Scanner(System.in);
+        RoomScreen roomScreen = new RoomScreen(scanner);
+        roomScreen.showRoomScreenOptions();
     }
 }
